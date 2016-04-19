026685ed4afb80b86a0c43ebedbbeab7128aaeb1
lawlite19 <849451478@qq.com>
2016-04-19 21:30:09 +0800

    爬取文档

---
+++ src/com/hhit/service/ISpiderDocumentService.java
@@ -0,0 +1,8 @@
+package com.hhit.service;
+
+import com.hhit.base.IDaoSupport;
+import com.hhit.entity.SpiderDocument;
+
+public interface ISpiderDocumentService extends IDaoSupport<SpiderDocument>{
+
+}
